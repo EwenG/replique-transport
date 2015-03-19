82774ff9eb6ccef8d771ef68b7a03245824dd197
ewen <ewen.grosjean@gmail.com>
2015-03-19 01:41:28 +0100

    Switch back to leiningen

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject ewen.replique/replique-transport "0.0.1-SNAPSHOT"
+            :description ""
+            :url ""
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :min-lein-version "2.0.0"
+            :source-paths ["src/clj"]
+            :test-paths ["test/clj"]
+            :resource-paths ["resources"]
+            :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                           [org.clojure/tools.nrepl "0.2.6"]])
